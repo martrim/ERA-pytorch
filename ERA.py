90653d7d9b3f8f6340226120d625d1db91e8d753
martrim <martin.trimmel@math.lth.se>
2023-08-22 19:45:41 +0200

    Simplified ERA.py.

--- ERA.py
+++ ERA.py
@@ -8,7 +8,7 @@ from scipy.optimize import minpack
 
 def get_rational_parameters(initialisation, degree_denominator, lower_bound=-3, upper_bound=3):
     print('Generating initial parameters.')
-    target_functions = {'leaky': nn.LeakyReLU, 'relu': nn.ReLU, 'swish': nn.SiLU, 'gelu': nn.GELU}
+    target_functions = {'leaky': lambda x: np.maximum(x, 0.3 * x), 'relu': lambda x: np.maximum(x, 0), 'swish': lambda x: x / (1 + np.exp(-x))}
 
     num_weights = 2 * degree_denominator + 2
     p0 = np.ones(num_weights, dtype='float32')
@@ -47,63 +47,50 @@ class ERA(torch.nn.Module):
                  denominator,
                  initialisation,
                  degree_denominator,
-                 input_shape,
-                 standardization_axes=None,
-                 learnable_axes=None,
+                 num_channels,
+                 data_type='image',
                  ):
         super(ERA, self).__init__()
 
-        # input_shape[0]: batch_size
-        # input_shape[-1]: num_channels
-        assert len(input_shape) in [2, 3, 4, 5]
-
-        self.normalise = False
         # Normalisation
-        if (standardization_axes is not None) or (learnable_axes is not None):
-            self.normalise = True
-            self.standardization_axes = standardization_axes
-            self.learnable_axes = learnable_axes
-            self.epsilon = 1e-6
-            self.new_shape = input_shape
-
-            weight_shape = []
-            for i in range(len(self.new_shape)):
-                if i in self.learnable_axes:
-                    weight_shape.append(self.new_shape[i])
-                else:
-                    weight_shape.append(1)
-            weight_shape = tuple(weight_shape)
-            self.beta = torch.nn.Parameter(torch.zeros(weight_shape))  # initialise with zeros
-            self.gamma = torch.nn.Parameter(torch.ones(weight_shape))  # initialise with ones
+        if data_type == 'image':
+            weight_shape = (1, num_channels, 1, 1)
+            self.standardization_axes = [1, 2]
+            self.learnable_axes = [3]
+        elif data_type == 'tokens':
+            weight_shape = (1, num_channels, 1)
+            self.standardization_axes = [2]
+            self.learnable_axes = []
+        elif data_type == 'flat':
+            weight_shape = (1, num_channels)
+            self.standardization_axes = [1]
+            self.learnable_axes = []  # [] or [1]
+        else:
+            raise ValueError('Invalid data_type.')
+        self.epsilon = 1e-6
+        self.beta = torch.nn.Parameter(torch.zeros(weight_shape))  # initialise with zeros
+        self.gamma = torch.nn.Parameter(torch.ones(weight_shape))  # initialise with ones
 
         # Activation Function
-        num_numerator_weights = degree_denominator + 2
-
-        weight_shape_ending = [1] * len(input_shape)
-        numerator_weight_shape = [num_numerator_weights] + weight_shape_ending
-        denominator_weight_shape = [degree_denominator] + weight_shape_ending
-        if initialisation != 'random':
-            numerator = torch.repeat_interleave(numerator, weight_shape_ending[-1], dim=-1)
-            denominator = torch.repeat_interleave(denominator, weight_shape_ending[-1], dim=-1)
-            numerator = torch.reshape(numerator, numerator_weight_shape)
-            denominator = torch.reshape(denominator, denominator_weight_shape)
-
         # Adding trainable weight vectors for numerator and denominator
         if initialisation == 'random':
-            num_initializer = torch.empty(numerator_weight_shape)
-            denom_initializer = torch.empty(denominator_weight_shape)
+            assert numerator is None
+            assert denominator is None
+            num_initializer = torch.empty(degree_denominator + 2)
+            denom_initializer = torch.empty(degree_denominator)
         else:
+            assert len(numerator) == degree_denominator + 2
+            assert len(denominator) == degree_denominator
             num_initializer = numerator
             denom_initializer = denominator
         self.numerator = torch.nn.Parameter(num_initializer)
         self.denominator = torch.nn.Parameter(denom_initializer)
 
-    def call(self, x):
-        if self.normalise:
-            # Normalisation
-            mu = torch.mean(x, self.standardization_axes, keepdim=True)
-            sigma = torch.std(x, self.standardization_axes, keepdim=True)
-            x = self.beta + self.gamma * (x - mu) / (sigma + self.epsilon)
+    def forward(self, x):
+        # Normalisation
+        mu = torch.mean(x, self.standardization_axes, keepdim=True)
+        sigma = torch.std(x, self.standardization_axes, keepdim=True)
+        x = self.beta + self.gamma * (x - mu) / (sigma + self.epsilon)
 
         # Activation Function
         return era_function(x, self.numerator, self.denominator)
